6daf3bdb404f5be1dfacf0a36865881c04f3bca6
OldM1lk <MrlaimWin@gmail.com>
2025-04-02 11:06:12 +0700

    Add entity, DAO and database

---
+++ app/src/main/java/com/example/lab_4/data/local/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.lab_4.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes")
+    fun getAllNotes(): List<Note>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note): Long
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("DELETE FROM notes")
+    suspend fun deleteAllNotes()
+}
