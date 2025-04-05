3f43a223d72b1881d4d2e1011eb91f7ce4fbb75e
OldM1lk <MrlaimWin@gmail.com>
2025-04-05 18:33:17 +0700

    Fix issues with di

--- app/src/main/java/com/example/lab_4/di/DatabaseModule.kt
+++ app/src/main/java/com/example/lab_4/di/DatabaseModule.kt
@@ -6,10 +6,15 @@ import com.example.lab_4.data.local.NoteDao
 import com.example.lab_4.data.local.NoteDatabase
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
     @Provides
+    @Singleton
     fun provideDatabase(application: Application): NoteDatabase {
         return Room.databaseBuilder(
             application,
@@ -19,6 +24,7 @@ object DatabaseModule {
     }
 
     @Provides
+    @Singleton
     fun provideNoteDao(database: NoteDatabase): NoteDao {
         return database.noteDao()
     }
